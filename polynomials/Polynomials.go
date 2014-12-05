7af0777147ffefed0768e37d6c6d740feced927b
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2014-12-05 16:58:18 -0600

    Functions to solve for real roots of polynomials up to order 4. Tests are exhaustive and all pass.

---
+++ polynomials/Polynomials.go
@@ -0,0 +1,158 @@
+package polynomials
+
+import "math"
+import "math/cmplx"
+
+//Solutions for the equation
+//
+//  x^2 + a x + b == 0
+//
+//The parameters are named in a nonstandard way so
+//that the formula generalizes more easily to qubics
+//and quartics. It is assumed that all the coefficients
+//are real, which means there are two cases. Either
+//both solutions are complex or both are real. 
+func QuadraticFormula(a, b float64) []float64 {
+  desc := a*a - 4 * b
+  if desc < 0 {
+    return []float64{}
+  } else {
+    s := math.Sqrt(desc)
+    return []float64{(a - s)/2., (a + s) / 2.}
+  }
+}
+
+//Solutions for the equation
+//
+//  x^3 + b x + a == 0
+//
+//This is a cubic equation in which all roots sum
+//to zero. There are two cases. Either there is one
+//real solution or three. 
+//
+//If desc is negative, then there are three real roots
+//and if it is positive, there is one. 
+//TODO: make some constants for the numbers in here. 
+func simplifiedCubicFormula(a, b float64) []float64 {
+  desc := a * a / 4. + b * b * b / 27.
+  //If the descriminant is negative, then there should be three real roots. 
+  if desc < 0 {
+    d := math.Sqrt(-desc)
+    p := cmplx.Pow(complex(-a/2., d), 1.0/3.0)
+    q := cmplx.Pow(complex(-a/2., -d), 1.0/3.0)
+    cw := complex(-1./2., -math.Sqrt(3)/2.)
+    ccw := complex(-1./2., math.Sqrt(3)/2.)
+    return []float64{real(p) + real(q), real(cw * p) + real(ccw * q), real(ccw * p) + real(cw * q)}
+  } else { //If it is positive, then there will only be one real root. 
+    d := math.Sqrt(desc)
+    r1 := -a/2. + d
+    r2 := -a/2. - d
+    var s1, s2 float64
+    if r1 < 0 {
+      s1 = -1;
+    } else {
+      s1 = 1;
+    }
+    if r2 < 0 {
+      s2 = -1;
+    } else {
+      s2 = 1;
+    }
+    return []float64{s1*math.Pow(s1*r1, 1.0/3.0) + s2*math.Pow(s2*r2, 1.0/3.0)}
+  }
+}
+
+//Solutions for the equation
+//
+//  x^3 + c x^2 + b x + a == 0
+//
+//TODO optimize the formula. 
+func CubicFormula(a, b, c float64) []float64 {
+  Z := simplifiedCubicFormula(a + 2*c*c*c/27. - (b*c)/3., b - c*c/3.)
+  for i := 0; i < len(Z); i++ {
+    Z[i] -= c/3.
+  }
+  return Z
+}
+
+//Solutions for the equation
+//
+//  x^4 + c x^2 + b x + a == 0
+//
+//Three possibilities: all solutions are complex, 
+//two solutions are real, or four solutions are real. 
+func simplifiedQuarticFormula(a, b, c float64) []float64 {
+  //The cubic equation comes from attempting to find something that completes the square
+  //so as to turn the quartic formula into a quadratic formula. It is always possible
+  //because a cubic equation with real coefficients will always have at least one real root.
+  //When there are zero or four real roots, then Z has three real roots, whereas
+  // when there are only two real roots, Z has only one real root. This can be proven
+  // by the fact that the descriminanent in the cubic formula comes out as a negative square
+  // for the cases in which there are zero or four real roots, and comes out as a positive
+  // square when there are two real roots. 
+  //It doesn't matter which root is used, but it appears always to be possible to choose
+  // a root which makes Ad and Bd positive. No proof for that either; should find one. 
+  Zl := CubicFormula((c * c * c - a * c - b * b / 4.0)/2., 2. * c * c - a, (5./2.) * c)
+  var Z, Bd float64
+
+  for _, Z = range Zl{
+    Bd = c + 2 * Z
+    if Bd >= 0 { break }
+  }
+
+  oo := c + Z
+  Ad := oo*oo - a
+
+  //It must be the case that Ad and Bd be real and have the same sign. Proof: 
+  //Since Z, c, and a are all real, it must be that Ad and Bd are real. 
+  //It must be that b == (+/-) 2 A B, and since b is real, the product of A and B must be real. 
+
+  A := math.Sqrt(Ad) //Ad and Bd now always positive (probably)
+  B := math.Sqrt(Bd) 
+
+  var bs float64
+  if b < 0 {
+    bs = -1
+  } else {
+    bs = 1
+  } //Necessary to ensure that -b / 2 == A B. 
+
+  //Ensure desc2 >= desc1. 
+  var desc1, desc2, s0 float64
+  if A >= 0 {
+    desc1 = Bd/4. - A - oo
+    desc2 = Bd/4. + A - oo
+    s0 = 1
+  } else {
+    desc1 = Bd/4. + A - oo
+    desc2 = Bd/4. - A - oo
+    s0 = -1
+  }
+
+  if desc2 >= 0 {
+    if desc1 >= 0 { //Four real solutions.
+      d1 := math.Sqrt(desc1)
+      d2 := math.Sqrt(desc2)
+      return []float64{s0*bs*B/2. - d1, s0*bs*B/2. + d1, -s0*bs*B/2. + d2, -s0*bs*B/2. - d2}
+    } else { //Two real solutions.
+      d := math.Sqrt(desc2)
+      return []float64{-s0*bs*B/2. + d, -s0*bs*B/2. - d} 
+    }
+  } else { //No real solutions.
+    return []float64{}
+  }
+}
+
+//Solutions for the equation
+//
+//  x^4 + d x^3 + c x^2 + b x + a == 0
+//
+//Three possibilities: all solutions are complex, 
+//two solutions are real, or four solutions are real. 
+func QuarticFormula(a, b, c, d float64) []float64 {
+  Z := simplifiedQuarticFormula(a - b*d/4. + c*d*d/16. - 3*d*d*d*d/256., b - c*d/2. + d*d*d/8., c - 3*d*d/8)
+  for i := 0; i < len(Z); i++ {
+    Z[i] -= d/4
+  }
+  return Z
+}
---
+++ polynomials/Polynomials_test.go
@@ -0,0 +1,279 @@
+package polynomials
+
+import "testing"
+import "sort"
+import "../test"
+
+var e float64 = .000001
+
+//In all these cases, it is important that the polynomial
+func TestQuadratic(t *testing.T) {
+  //Two cases: zero real roots or two roots. 
+  //case 1, zero real roots. In this case the complex
+  //roots are of the form r +/- i s.
+  for i := 0; i < 10; i++ {
+    p := test.RandFloat(-100, 100)
+    q := test.RandFloat(-100, 100)
+
+    ans := QuadraticFormula(2 * p, p * p + q * q)
+
+    //ans should be empty. 
+    if len(ans) != 0 {
+      t.Error("quadratic formula error: case 1, expected no real roots for p = ", p , ", q = ", q, "; ans = ", ans)
+    }
+  }
+
+  //case 2: two real roots. 
+  for i := 0; i < 10; i++ {
+    p := test.RandFloat(-100, 100)
+    q := test.RandFloat(-100, 100)
+
+    ans := QuadraticFormula(p + q, p * q)
+
+    if len(ans) != 2 {
+      t.Error("quadratic formula error: case 2, expected two real roots for p = ", p , ", q = ", q, "; ans = ", ans)
+    } else {
+      test_list := []float64{p, q}
+      sort.Float64s(test_list)
+      sort.Float64s(ans)
+
+      var close_enough bool = true
+      for i := 0; i < len(test_list); i++ {
+        close_enough = close_enough && test.CloseEnough(test_list[i], ans[i], e);
+      }
+
+      if !close_enough {
+        t.Error("quadratic formula error: case 2, roots do not match for p = ", p , ", q = ", q, "; ans = ", ans)
+      }
+    }
+  }
+}
+
+func TestCubic(t *testing.T) {
+
+  //In the simplified cubic formala, all roots sum to zero. 
+  //Two cases: one root or three roots.
+  //case 1: In this case, the three roots are p, (-p + i q) / 2, (-p - i q) / 2. 
+  for i := 0; i < 10; i++ {
+    p := test.RandFloat(-100, 100)
+    q := test.RandFloat(-100, 100)
+
+    ans := simplifiedCubicFormula(-(p*p*p + p*q*q)/4., (-3.*p*p + q*q)/4.)
+
+    if len(ans) != 1 {
+      t.Error("simplified cubic formula error: case 1, expected one real root for p = ",
+        p , ", q = ", q, "; ans = ", ans)
+    } else if !test.CloseEnough(ans[0], p, e) {
+      t.Error("simplified cubic formula error: case 1, expected real roots to match p = ",
+        p , ", q = ", q, "; ans = ", ans)
+    }
+
+    //Now add a constant to the roots and test them against the more general formula. 
+    r := test.RandFloat(-100, 100)
+    ans2 := CubicFormula(-(p*p*p + p*q*q - 3.*p*p*r + q*q*r)/4. - r*r*r, (-3.*p*p + q*q)/4. + 3.*r*r, -3.*r)
+
+    if len(ans2) != 1 {
+      t.Error("cubic formula error: case 1, expected one real root for p = ",
+        p , ", q = ", q, ", r = ", r, "; ans = ", ans2)
+    } else if !test.CloseEnough(ans[0], ans2[0] - r, e) {
+      t.Error("cubic formula error: case 1, expected real root to match p = ",
+        p , ", q = ", q, ", r = ", r, "; ans = ", ans, "; ans2 = ", ans2)
+    }
+  }
+
+  //case 2: In this case the roots are p, q, and -p - q. 
+  for i := 0; i < 10; i++ {
+    p := test.RandFloat(-100, 100)
+    q := test.RandFloat(-100, 100)
+
+    ans := simplifiedCubicFormula(p*p*q+p*q*q, -p*p-p*q-q*q)
+
+    if len(ans) != 3 {
+      t.Error("simplified cubic formula error: case 2, expected three real roots for p = ",
+        p , ", q = ", q, "; ans = ", ans)
+    } else {
+      test_list := []float64{p, q, -p - q}
+      sort.Float64s(test_list)
+      sort.Float64s(ans)
+
+      var close_enough bool = true
+      for i := 0; i < len(test_list); i++ {
+        close_enough = close_enough && test.CloseEnough(test_list[i], ans[i], e);
+      }
+
+      if !close_enough {
+        t.Error("simplified cubic formula error: case 2, expected real roots to match p = ",
+          p , ", q = ", q, "; ans = ", ans)
+      }
+    }
+
+    //Now add a constant to the roots and test them against the more general formula. 
+    r := test.RandFloat(-100, 100)
+    ans2 := CubicFormula(p*p*q+p*q*q+p*p*r+p*q*r+q*q*r-r*r*r, -p*p-p*q-q*q + 3.*r*r, -3.*r)
+
+    if len(ans2) != 3 {
+      t.Error("cubic formula error: case 2, expected one real root for p = ",
+        p , ", q = ", q, ", r = ", r, "; ans = ", ans2)
+    } else {
+      //move the new roots back to being centered at zero; this should put them in the same position
+      //as the roots in the simplified formula. 
+      for i := 0; i < 3; i++ {
+        ans2[i] -= r;
+      }
+      sort.Float64s(ans2);
+
+      var close_enough bool = true;
+      for i := 0; i < len(ans); i++ {
+        close_enough = close_enough && test.CloseEnough(ans[i], ans2[i], e)
+      }
+
+      if !close_enough {
+        t.Error("cubic formula error: case 2, expected real root to match p = ",
+          p , ", q = ", q, ", r = ", r, "; ans = ", ans2)
+      }
+    }
+  }
+}
+
+func TestSimplifiedQuartic(t *testing.T) {
+  var b_positive, b_negative int
+
+  //Three cases: zero roots, two roots, or one root. 
+  //case 1: The roots are p + i q, p - i q, -p + i r, -p - i r and b >= 0. 
+  //Since b == 2 p (q^2 - r^2), this happens when p has the same sign as q^2 - r^2
+  for i := 0; i < 10; i++ {
+    //Simplified formula
+    p := test.RandFloat(-100, 100)
+    q := test.RandFloat(-100, 100)
+    r := test.RandFloat(-100, 100)
+
+    a := p*p*p*p + p*p*q*q + p*p*r*r + q*q*r*r
+    b := 2*p*q*q - 2*p*r*r
+    c := -2*p*p + q*q + r*r
+
+    if b > 0 {
+      b_positive ++ 
+    } else { 
+      b_negative ++
+    }
+
+    ans := simplifiedQuarticFormula(a, b, c)
+
+    if len(ans) != 0 {
+      t.Error("simplified quartic formula error: case 1, expected zero real roots for p = ",
+        p , ", q = ", q, ", r = ", r, "; ans = ", ans)
+    } 
+  }
+
+  //case 2: The roots are p, q, (-p - q)/2 + i r, (-p - q)/2 - i r 
+  // b == -(p - q)^2 (p + q)/4 - (p - q) r^2
+  for i := 0; i < 10; i++ {
+    //Simplified formula
+    p := test.RandFloat(-100, 100)
+    q := test.RandFloat(-100, 100)
+    r := test.RandFloat(-100, 100)
+
+    a := p*p*p*q/4. + p*p*q*q/2. + p*q*q*q/4.+p*q*r*r
+    b := -p*p*p/4.+p*p*q/4.+p*q*q/4.-q*q*q/4.-p*r*r-q*r*r
+    c := -3.*p*p/4.-p*q/2.-3.*q*q/4.+r*r
+
+    ans := simplifiedQuarticFormula(a, b, c)
+
+    if len(ans) != 2 {
+      t.Error("simplified quartic formula error: case 2, expected two real roots for p = ",
+        p , ", q = ", q, ", r = ", r, "; ans = ", ans)
+    } else {
+      test_list := []float64{p, q}
+      sort.Float64s(test_list)
+      sort.Float64s(ans)
+
+      var close_enough bool = true
+      for i := 0; i < len(test_list); i++ {
+        close_enough = close_enough && test.CloseEnough(test_list[i], ans[i], e);
+      }
+
+      if !close_enough {
+        t.Error("simplified quartic formula error: case 2, expected two roots to match p = ",
+          p , ", q = ", q, ", r = ", r, "; ans = ", ans)
+      }
+    }
+  }
+
+  //case 3: The roots are p, q, r, -p - q - r
+  // b == (p + q)(p + r)(q + r)
+  for i := 0; i < 10; i++ {
+    //Simplified formula
+    p := test.RandFloat(-100, 100)
+    q := test.RandFloat(-100, 100)
+    r := test.RandFloat(-100, 100)
+
+    a := -p*p*q*r - p*q*q*r - p*q*r*r
+    b := p*p*q + p*p*r + p*q*q + p*r*r + q*q*r + q*r*r + 2*p*q*r
+    c := -p*p - p*q - p*r - q*q - q*r - r*r
+
+    ans := simplifiedQuarticFormula(a, b, c)
+
+    if len(ans) != 4 {
+      t.Error("simplified quartic formula error: case 3, expected four real roots for p = ",
+        p , ", q = ", q, ", r = ", r, "; ans = ", ans)
+    } else {
+      test_list := []float64{p, q, r, -p-q-r}
+      sort.Float64s(test_list)
+      sort.Float64s(ans)
+
+      var close_enough bool = true
+      for i := 0; i < len(test_list); i++ {
+        close_enough = close_enough && test.CloseEnough(test_list[i], ans[i], e);
+      }
+
+      if !close_enough {
+        t.Error("simplified quartic formula error: case 3, expected four roots to match p = ",
+          p , ", q = ", q, ", r = ", r, "; ans = ", ans)
+      }
+    }
+  }
+}
+
+//Tests only the four root case because it shouldn't make a difference here. 
+func TestQuartic(t *testing.T) {
+
+  //The roots are p, q, r, -p - q - r
+  for i := 0; i < 10; i++ {
+    p := test.RandFloat(-100, 100)
+    q := test.RandFloat(-100, 100)
+    r := test.RandFloat(-100, 100)
+    s := test.RandFloat(-100, 100)
+
+    a := -p*q*r*(p+q+r)
+    b := (p + q)*(p + r)*(q + r)
+    c := -p*p - p*q - p*r - q*q - q*r - r*r
+
+    af := -(p + q + r - s)*(p + s)*(q + s)*(r + s)
+    bf := p*p*q + p*p*r + p*q*q + p*r*r + q*q*r + q*r*r + 2*(-2*s*s*s + p*p*s + p*q*r + p*q*s + p*r*s + q*q*s + q*r*s + r*r*s)
+    cf := -p*p - p*q - p*r - q*q - q*r - r*r + 6*s*s
+    df := -4*s
+
+    ans := simplifiedQuarticFormula(a, b, c)
+    ans_full := QuarticFormula(af, bf, cf, df)
+
+    if len(ans) != len(ans_full) { 
+      t.Error("quartic formula error: mismatched number of roots; ans = ", ans, ", ans_full = ", ans_full)
+    } else {
+      //move the new roots back to being centered at zero; this should put them in the same position
+      //as the roots in the simplified formula. 
+      for i := 0; i < len(ans); i++ {
+        ans_full[i] -= s;
+      }
+
+      var close_enough bool = true;
+      for i := 0; i < len(ans); i++ {
+        close_enough = close_enough && test.CloseEnough(ans[i], ans_full[i], e)
+      }
+
+      if !close_enough {
+        t.Error("quartic formula error: expected roots to match p = ",
+          p , ", q = ", q, ", r = ", r, ", s = ", s,"; ans = ", ans_full)
+      }
+    }
+  }
+}
